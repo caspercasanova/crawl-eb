3c58baf641ce54bc0c8b0bd4e782ec3c61813ea4
nick <nhjlopez@gmail.com>
2021-05-22 20:46:57 -0700

    First Commit

---
+++ goals.md
@@ -0,0 +1,16 @@
+Scrape Events
+  - Eventbrite
+  - BandsInTown
+  - Meetup
+  - Yelp
+General DataStructures
+
+An Event 
+  EventOrganizer
+  Date: 
+  Price: || 0
+  Location
+  About
+  Category
+  Format
+  Tags
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// npx playwright codegen eventbrite.com
+
+// need a click back button
+// https://playwright.dev/docs/navigations/
+// need a scroll down
+
+// meeetup
+//location xpath     //*[@id="eventHome--wvlqnqycchbdc"]/div[1]/div[1]/div/section/div[2]/div/section/div[2]/div[2]/div/a/address
+//detailts xpath     //*[@id="eventHome--wvlqnqycchbdc"]/div[1]/div[2]/div/div/section[1]/div/div[2]
+//timeDetails xpath   //*[@id="eventHome--wvlqnqycchbdc"]/div[1]/div[1]/div/section/div[2]/div/section/div[1]/div/div[2]/div
